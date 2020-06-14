3eeb82ceec811d45336ebf686680288759061ec9
ekb1zh <ekb1zh@gmail.com>
2020-06-15 00:12:59 +0300

    Updates

--- middle/types.d.ts
+++
@@ -1,6 +0,0 @@
-export type Data = {
-  name: string,
-  email: string,
-  phone: string,
-  message: string,
-}
---
+++ precache-manifest.f6c880b776dfdfb8ab7214e5fd3dff74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4295b75cdc23325c86a3c3ac7fab520e",
+    "url": "/madappgang/index.html"
+  },
+  {
+    "revision": "ef9d04dd672db36c94df",
+    "url": "/madappgang/static/js/2.329da7f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/madappgang/static/js/2.329da7f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a6164b3a81e66947581",
+    "url": "/madappgang/static/js/main.7ba6e262.chunk.js"
+  },
+  {
+    "revision": "7e30d4a3b35a53c5d9b2",
+    "url": "/madappgang/static/js/runtime-main.dfe9c123.js"
+  }
+]);
